fd2b80894ecc2b0800e9b8d62dd8fd8dadb945e6
ANA LIANGXIN HERREZUELO HERRERO <anal.herher@educa.jcyl.es>
2024-11-21 12:06:23 +0100

    21/11/2024

--- WU2_Basics/src/ArrayOrdenation.java
+++ WU2_Basics/src/ArrayOrdenation.java
@@ -5,22 +5,54 @@ public class ArrayOrdenation {
 		Scanner keyboard = new Scanner(System.in);
 		
 		
-		/* BUBBLE METHOD ORDENATION*/
-		int array [] = {1,2,3,4,5,6};
+		/* BUBBLE METHOD ORDENATION it works
+		int array [] = {5,2,4,3,8,9};
 		int max, aux;
 		
 		for (int i=0; i<array.length;i++) {
 			for (int j=0; j<array.length-1;j++) {
-				if(array[i]>array[i+1]) {
-					aux=array[i];
-				
+				if(array[j]>array[j+1]) {
+					aux=array[j];
+					array[j]=array[j+1];
+					array[j+1]=aux;
 				}
-			}
-		}
+			}//for-j
+			
+			for(int k=0;k<array.length;k++) {
+			System.out.print(array[k]+" ");
+			}//for-k
+			System.out.println(" ");
+		}//for-i
+		*/
 		
-		for(int k=0;k<array.length;k++) {
-			System.out.print(array[k]);
-		}
+		
+		
+		
+		
+		
+		 
+		/* BUBBLE METHOD ORDENATION */
+		int array [] = {5,2,4,3,8,9};
+		int max, aux;
+		
+		for (int i=0; i<array.length;i++) {
+			for (int j=0; j<array.length-1;j++) {
+				if(array[j]>array[j+1]) {
+					aux=array[j];
+					array[j]=array[j+1];
+					array[j+1]=aux;
+				}
+			}//j
+			
+			for(int k=0;k<array.length;k++) {
+			System.out.print(array[k]+" ");
+			}//k
+			System.out.println(" ");
+		}//i
+		
+		
+		
+		
 		
 		
 		
