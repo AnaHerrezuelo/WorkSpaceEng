69a12e8415a2819cfb2e1510acae55de2229e417
ANA LIANGXIN HERREZUELO HERRERO <anal.herher@educa.jcyl.es>
2024-12-12 10:54:33 +0100

    actu

---
+++ WU2_Basics/src/Exercises/Act05.java
@@ -0,0 +1,10 @@
+package Exercises;
+
+public class  Act05 {
+	public static void main (String args[]) {
+	final float EURO= 166.38621f;
+		System.out.println("The value of a euro in pesetas is: " + EURO);
+		//EURO=100;  
+		System.out.println("\n\nFinal del programa"); 
+	}
+}
---
+++ WU2_Basics/src/Exercises/Act16.java
@@ -0,0 +1,14 @@
+package Exercises;
+import java.util.Scanner;
+
+public class Act16 {
+	public static void main(String[] args) {
+		float num;
+		
+		Scanner keyboard = new Scanner(System.in);
+		System.out.print("Enter a number: ");
+		num=keyboard.nextFloat();
+        System.out.println(num);
+	};
+};
+
