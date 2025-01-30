56dcb9d50c4846f393336ed4b6cd6a2ccea57920
ANA LIANGXIN HERREZUELO HERRERO <anal.herher@educa.jcyl.es>
2025-01-30 12:09:42 +0100

    30/01/2025

---
+++ WU2_Basics/src/Objects/bucket.java
@@ -0,0 +1,114 @@
+package Objects;
+
+public class bucket {
+	private int capacity, capFill;
+	private String color, material;
+	private boolean handle = true;
+	
+	
+	public bucket(int capacity, String color, String material, boolean handle) {
+		super();
+		setCapacity(capacity);
+		this.color = color;
+		this.material = material;
+		this.handle = handle;
+	}
+	
+	public bucket( String color, String material, boolean handle) {
+		super();
+		this.color = color;
+		this.material = material;
+		this.handle = handle;
+	}
+	
+	public void showBucket() {
+		System.out.println("#      #");
+		for(int i=0; i < capacity; i++) {
+			//System.out.println("#      #");
+			if(capFill>i) {
+				System.out.println("#~~~~~~#");
+			}
+		}
+		System.out.println("########");
+	}
+	
+	public void fillBucket() {
+		capFill=capacity;
+
+	}
+	
+	public void extractLiquid(int drain){
+		setCapFill(capFill-drain);
+	}	
+
+	
+	
+	
+	
+	
+	//GETTERS AND SETTERS
+	public int getCapacity() {
+		return capacity;
+	}
+	public void setCapacity(int capacity) {
+		if(capacity>0) {
+			this.capacity=capacity;
+		}else {
+			System.err.println("Negative");
+			this.capacity = 0;			
+		}
+
+	}
+	public int getCapFill() {
+		return capFill;
+	}
+	public void setCapFill(int capFill) {
+		if(capFill>0) {
+			this.capFill=capFill;
+		}else {
+			System.err.println("Not enought liquid");
+			this.capFill = 0;			
+		}
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getMaterial() {
+		return material;
+	}
+	public void setMaterial(String material) {
+		this.material = material;
+	}
+	public boolean isHandle() {
+		return handle;
+	}
+	public void setHandle(boolean handle) {
+		this.handle = handle;
+	}
+	
+	
+	
+	//PRINT
+	public void printToString() {
+		System.out.println(toString());
+	}
+
+	
+	@Override
+	public String toString() {
+		return "bucket [capacity=" + capacity + ", capFill=" + capFill + ", color=" + color + ", material=" + material
+				+ ", handle=" + handle + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+}// end public class bucket 
--- WU2_Basics/src/Objects/floor.java
+++ WU2_Basics/src/Objects/floor.java
@@ -3,10 +3,13 @@ package Objects;
 public class floor {
 	public static void main(String[] args) {
 		
+		System.out.println("Flat1 --- ");
 		mainLightBulb flat1=new mainLightBulb();
 		flat1.inici();
 		flat1.printFlat();
+		System.out.println();
 		
+		System.out.println("Flat2 --- ");
 		mainLightBulb flat2=new mainLightBulb();
 		flat2.inici();
 		flat2.printFlat();
