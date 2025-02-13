e659c9580c0e5cd1dda0f551075f3333ddec8770
ANA LIANGXIN HERREZUELO HERRERO <anal.herher@educa.jcyl.es>
2025-02-13 11:25:56 +0100

    descarga en otro pc

---
+++ Inheritance_Exercises/src/Casino/MainCasino.java
@@ -0,0 +1,8 @@
+package Casino;
+
+public class MainCasino {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}//	end public static void main(String[] args)
+}// end public class MainCasino
