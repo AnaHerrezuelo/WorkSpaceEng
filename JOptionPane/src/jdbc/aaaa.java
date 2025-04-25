6336e1294eb7c9afb259cb1e66362d7df7a62bde
ANA LIANGXIN HERREZUELO HERRERO <anal.herher@educa.jcyl.es>
2025-04-25 10:04:27 +0200

    25/04/2025

--- JOptionPane/src/jdbc/aaaa.java
+++ JOptionPane/src/jdbc/aaaa.java
@@ -1,8 +1,41 @@
 package jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+
 public class aaaa {
 	public static void main(String[] args) {
 		
+		String connector = "com.mysql.jdbc.Driver";
+//		String url = "jdbc:mysql://10.195.136.234:3306/tienda";
+		String url = "jdbc:mysql://10.195.136.234:3306/jardineria";
+		String user = "root";
+		String pass = "";
 		
-	}
-}
+		try {
+			Connection connection = DriverManager.getConnection(url, user, pass);
+			java.sql.Statement stmnt = connection.createStatement();
+//			System.out.println("connect");
+			
+			
+//			String query = "SELECT * FROM `clientes`";
+//			ResultSet result = stmnt.executeQuery(query);
+//
+//			while(result.next()) {
+//				System.out.print(result.getString(1) + " - ");
+//				System.out.print(result.getString(2) + " - ");
+//				System.out.println(result.getString(3));
+//			}
+			
+			
+			
+				
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}// end public static void main(String[] args) 
+}//end public class aaaa
